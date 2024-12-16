1e83fc7e2d48463271c487dd86a95068b70d92b0
xaralien <dimasuzumaki126@gmail.com>
2024-12-16 09:55:10 +0700

    update database

--- application/config/config.php
+++ application/config/config.php
@@ -386,15 +386,22 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
+// $config['sess_driver'] = 'files';
+// $config['sess_cookie_name'] = 'boc-session';
+// $config['sess_samesite'] = 'Lax';
+// $config['sess_expiration'] = 86400;
+// $config['sess_save_path'] = NULL;
+// $config['sess_match_ip'] = FALSE;
+// $config['sess_time_to_update'] = 300;
+// $config['sess_regenerate_destroy'] = FALSE;
+
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'boc-session';
-$config['sess_samesite'] = 'Lax';
-$config['sess_expiration'] = 86400;
-$config['sess_save_path'] = NULL;
+$config['sess_cookie_name'] = 'ci_session';
+$config['sess_expiration'] = 43200;
+$config['sess_save_path'] = sys_get_temp_dir();
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
-
 /*
 |--------------------------------------------------------------------------
 | Cookie Related Variables
